1ab3e3be0411f85879c182d9a7e00df692fa4311
your name here <mmakhambetov@yandex.ru>
2021-10-13 16:48:41 +0400

    Build user interface

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,26 @@
 // Imports
+import Board from './board.js';
+
 let board = new Board();
 
 window.onload = () => {
     const grid = board.grid;
     console.log(grid);
+
+    const gridDiv = document.createElement('div');
+    gridDiv.setAttribute('class', 'grid');
+    document.body.appendChild(gridDiv);
+
+    const gridContainer = document.createElement('div');
+    gridContainer.setAttribute('class', 'grid-container');
+    grid.map(row => {
+        return row.map(cell => {
+            const gridItem = document.createElement('div');
+            gridItem.setAttribute('data', cell);
+            gridItem.setAttribute('class', 'grid-item');
+            gridContainer.appendChild(gridItem);
+        });
+    });
+    gridDiv.appendChild(gridContainer);
+    
 };
